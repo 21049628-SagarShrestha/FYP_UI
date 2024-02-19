c48bf6e8350475f949efbefeee6da19e137a7e25
sagarstha254 <sagarstha254@gmail.com>
2024-02-19 22:45:34 +0545

    feat(UI): view destination

---
+++ src/pages/destination.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useGetDestinationsQuery } from "@/api/api";
+import { useNavigate } from "react-router-dom";
+
+const hotels = () => {
+  const { data: response, error, isLoading } = useGetDestinationsQuery();
+  const destinations = response?.destinations || [];
+  const navigate = useNavigate();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching Destinations</p>;
+  }
+  return (
+    <div>
+      <div>
+        {destinations &&
+          destinations.map((i) => {
+            return (
+              <div key={i._id}>
+                <img
+                  src={i.image}
+                  // src={`${api_port}images/hotels/${i.image}`}
+                  alt={i.name}
+                  height={90}
+                  width={90}
+                />
+                <div>
+                  {i.name}
+                  {i.location}
+                  {i.description}
+                  {i.rating}
+                  {i.placesToVisit}
+                  {i.cost}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default hotels;
