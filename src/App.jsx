b85cda344c451e738fe3f13b24c1750a3b2bd00c
sagarstha254 <sagarstha254@gmail.com>
2024-02-19 22:46:36 +0545

    fix: manage routes to multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 
 //import all routes as AppRoutes
 import { AppRoutes } from "./routes";
+import Header from "./components/Common/Header.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <AppRoutes />
     </BrowserRouter>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,14 +2,35 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 //import routes
-import SignIn from "./pages/signIn";
+import Index from "./pages/index";
+import Error from "./components/Common/Error";
 import AdminHotel from "./pages/adminHotel";
 import AdminDestination from "./pages/adminDestination";
-//set routes to different pages
+import AdminAdventrue from "./pages/adminAdventrue";
+import AdminTransport from "./pages/adminTransport";
+
+//import user routes
+import Adventure from "./pages/adventure";
+import Destination from "./pages/destination";
+import Transport from "./pages/transport";
+import Hotels from "./pages/hotels";
+import SignIn from "./pages/signIn";
+import Rooms from "./pages/rooms";
+
+//set routes to respective pages
 export const AppRoutes = () => (
   <Routes>
-    <Route path="signin" element={<SignIn />} />
+    <Route path="/" element={<Index />} />
     <Route path="adminHotel" element={<AdminHotel />} />
     <Route path="adminDestination" element={<AdminDestination />} />
+    <Route path="adminAdventure" element={<AdminAdventrue />} />
+    <Route path="adminTransport" element={<AdminTransport />} />
+    <Route path="adventure" element={<Adventure />} />
+    <Route path="destination" element={<Destination />} />
+    <Route path="transport" element={<Transport />} />
+    <Route path="hotels" element={<Hotels />} />
+    <Route path="signin" element={<SignIn />} />
+    <Route path="rooms" element={<Rooms />} />
+    <Route path="*" element={<Error />} />
   </Routes>
 );
