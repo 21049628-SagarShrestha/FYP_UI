ca0c007e502d402989e9c0e0b0d8ce8f09de53a5
sagarstha254 <sagarstha254@gmail.com>
2024-02-19 22:41:40 +0545

    feat(UI): admin Page to CRUD Destination

--- src/pages/adminDestination.jsx
+++ src/pages/adminDestination.jsx
@@ -1,10 +1,12 @@
 // ExampleComponent.js
-import React from "react";
-import { useGetDestinationsQuery } from "../api/api";
+import React, { useState } from "react";
+import { useGetDestinationsQuery } from "@/api/api";
+import AddDestination from "@/components/AddDestination";
 
 const adminDestination = () => {
   const { data: response, error, isLoading } = useGetDestinationsQuery();
   const destinations = response?.destinations || [];
+  const [showButton, setShowButton] = useState(false);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -17,15 +19,51 @@ const adminDestination = () => {
   return (
     <div>
       <h1>Destinations</h1>
-      <ul>
-        {destinations &&
-          destinations.map((x) => (
-            <li key={x._id}>
-              {x.name}
-              {x.location}
-            </li>
-          ))}
-      </ul>
+      {showButton && <AddDestination />}
+      <button onClick={() => setShowButton(true)}> Add More Destination</button>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Image</td>
+              <td>Name</td>
+              <td>Location</td>
+              <td>Description</td>
+              <td>Rating</td>
+              <td>Places TO Visit</td>
+              <td>Cost</td>
+              <td colSpan="2">Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            {destinations &&
+              destinations.map((i) => {
+                return (
+                  <tr key={i._id}>
+                    <td>
+                      <img src={i.image} alt={i.name} height={90} width={90} />
+                    </td>
+                    <td>{i.name}</td>
+                    <td>{i.location}</td>
+                    <td>{i.description}</td>
+                    <td>{i.rating}</td>
+                    <td>{i.placesToVisit}</td>
+                    <td>{i.cost}</td>
+                    {/* <td>
+                      <a onClick={() => setRooms(i._id)}>Add Room</a>
+                        <a
+                        onClick={() => remove(i._id)}
+                        className={styles.delbtn}
+                      >
+                        Delete
+                      </a>
+                    </td> */}
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
