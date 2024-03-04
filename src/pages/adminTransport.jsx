c69660746be2eb6cd9f1ad882670ef3c69e8e0d4
sagarstha254 <sagarstha254@gmail.com>
2024-03-04 23:38:32 +0545

    feat(UI): admin panel to view flights

--- src/pages/adminTransport.jsx
+++ src/pages/adminTransport.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+// ExampleComponent.js
+import React, { useState } from "react";
+import { useGetFlightsQuery } from "@/api/api";
+import Table from "../components/Common/Table";
 
 const adminTransport = () => {
-  return <div>adminTransport</div>;
+  const {
+    data: { Flights: flights } = {},
+    error,
+    isLoading,
+  } = useGetFlightsQuery();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching Flights</p>;
+  }
+
+  const data = flights || [];
+
+  // Add an index to each flight,
+  const indexedData = data.map((flight, index) => ({
+    ...flight,
+    index: index + 1,
+  }));
+
+  const columns = [
+    {
+      Header: "Index",
+      accessor: "index",
+    },
+    {
+      Header: "Flight Date",
+      accessor: "flightDate",
+    },
+    {
+      Header: "Airline",
+      accessor: "airline",
+    },
+    {
+      Header: "Departure Airport",
+      accessor: "departure_airport",
+    },
+    {
+      Header: "Arrival Airport",
+      accessor: "arrival_airport",
+    },
+    {
+      Header: "Departure Time",
+      accessor: "departure_time",
+    },
+    {
+      Header: "Flight Duration",
+      accessor: "flight_time",
+    },
+    {
+      Header: "Price",
+      accessor: "price",
+    },
+  ];
+
+  return (
+    <div>
+      <Table columns={columns} data={indexedData} />
+    </div>
+  );
 };
 
 export default adminTransport;
