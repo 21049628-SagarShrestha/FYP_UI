5bdc5d6d8558c2e205b76da3c1e9a8398055f22b
sagarstha254 <sagarstha254@gmail.com>
2024-03-21 23:08:49 +0545

    feat(profile): display user information

---
+++ src/pages/profile.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { useGetUsersQuery } from "../api/api";
+import ChangePassword from "../components/ChangePassword";
+import "@/assets/styles/profile.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+} from "../state/slices/userSlice";
+import { useNavigate } from "react-router-dom";
+
+const profile = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const { data: { Users: users } = {}, error, isLoading } = useGetUsersQuery();
+  const reqUser = users?.filter((user) => user.email === currentUser.email)[0];
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+
+      const response = await fetch(`http://localhost:8081/signout`, {
+        method: "GET",
+        credentials: "include", // Include credentials (cookies) with the request
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess());
+    } catch (error) {
+      dispatch(signOutUserFailure());
+    }
+  };
+  return (
+    <>
+      <div className="main-wrapper">
+        <div className="wrapper">
+          <h1 className="font-semibold">Login</h1>
+          {reqUser ? (
+            <div className="user-container">
+              <div>
+                <p>User Name: {reqUser.userName}</p>
+                <p>Email: {reqUser.email}</p>
+                <p>Role: {reqUser.role}</p>
+              </div>
+              <div>
+                <button onClick={() => navigate("/changePassword")}>
+                  Change Password
+                </button>
+                {currentUser && (
+                  <span
+                    onClick={handleSignOut}
+                    className="text-red-700 cursor-pointer"
+                  >
+                    Sign out
+                  </span>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div>Loading..</div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default profile;
