e6dc71db3c6b7540ee505f3bccbeb8545e7f4a7a
sagarstha254 <sagarstha254@gmail.com>
2024-04-07 11:03:49 +0545

    feat(adventure): add adventure id to database and css

--- src/components/Adventure/AdventureReservation.jsx
+++ src/components/Adventure/AdventureReservation.jsx
@@ -3,7 +3,7 @@ import { useForm, useWatch } from "react-hook-form";
 import ConfirmAdventure from "./ConfirmAdvneture";
 import { useEffect } from "react";
 
-const AdventureReservation = ({ events }) => {
+const AdventureReservation = ({ events, adventureBookId }) => {
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [price, setPrice] = useState("");
 
@@ -27,7 +27,7 @@ const AdventureReservation = ({ events }) => {
     }
   }, [selectedEvent]);
   return (
-     <div>
+    <>
       {showConfirmation ? (
         <ConfirmAdventure
           eventDate={watch("eventDate")}
@@ -35,57 +35,65 @@ const AdventureReservation = ({ events }) => {
           phone={watch("phone")}
           event={watch("event")}
           price={price}
+          adventureId={adventureBookId}
         />
       ) : (
-        <div className="border border-black  rounded-lg p-3 max-w-4xl mx-auto my-10">
-          <h3 className="text-center font-extrabold uppercase">Make Bookings</h3>
-          <br/>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white  border border-black  rounded-lg p-3 max-w-4xl mx-auto my-10">
+            <h3 className="text-center font-extrabold uppercase">
+              Make Bookings
+            </h3>
+            <br />
 
-          <form className="flex flex-col sm:flex-row gap-4" onSubmit={handleSubmit(submitAlbum)}>
-            <div className="flex flex-col gap-4 flex-1">
-              <label className="font-semibold">Event Date :</label>
-              <input
-                className="border p-3 rounded-lg "
+            <form
+              className="flex flex-col sm:flex-row gap-4"
+              onSubmit={handleSubmit(submitAlbum)}
+            >
+              <div className="flex flex-col gap-4 flex-1">
+                <label className="font-semibold">Event Date :</label>
+                <input
+                  className="border p-3 rounded-lg "
+                  {...register("eventDate", { required: true })}
+                  type="Date"
+                  min={new Date().toISOString().split("T")[0]} // Set min attribute to current date
+                />
+                {errors.eventDate && <p>eventDate is required</p>}
 
-                {...register("eventDate", { required: true })}
-                type="Date"
-                min={new Date().toISOString().split("T")[0]} // Set min attribute to current date
-              />
-              {errors.eventDate && <p>eventDate is required</p>}
+                <label className="font-semibold">User name : </label>
+                <input
+                  className="border p-3 rounded-lg "
+                  {...register("userName", { required: true })}
+                  type="text"
+                />
+                {errors.userName && <p>userName is required</p>}
 
-              <label className="font-semibold">User name : </label>
-              <input 
-                className="border p-3 rounded-lg "
+                <label className="font-semibold">Phone : </label>
+                <input
+                  className="border p-3 rounded-lg "
+                  {...register("phone", { required: true })}
+                  type="Number"
+                />
+                {errors.phone && <p>phone is required</p>}
+              </div>
+              <div className="flex flex-col flex-1 gap-4">
+                <label className="font-semibold">Choose Event: </label>
+                <select
+                  className="border p-3 rounded-lg "
+                  {...register("event", { required: true })}
+                >
+                  <option value="" disabled>
+                    Select an event
+                  </option>
+                  {events &&
+                    events.map((event, index) => (
+                      <option key={index} value={event.eventName}>
+                        {event.eventName}
+                      </option>
+                    ))}
+                </select>
+                {errors.event && <p>Event is required</p>}
 
-                {...register("userName", { required: true })}
-                type="text"
-              />
-              {errors.userName && <p>userName is required</p>}
-
-              <label className="font-semibold">Phone : </label>
-              <input
-              className="border p-3 rounded-lg "
-               {...register("phone", { required: true })} type="Number" />
-              {errors.phone && <p>phone is required</p>}
-            </div>
-            <div className="flex flex-col flex-1 gap-4">
-              <label className="font-semibold">Choose Event: </label>
-              <select 
-              className="border p-3 rounded-lg "
-              {...register("event", { required: true })}>
-                <option value="" disabled>
-                  Select an event
-                </option>
-                {events &&
-                  events.map((event, index) => (
-                    <option key={index} value={event.eventName}>
-                      {event.eventName}
-                    </option>
-                  ))}
-              </select>
-              {errors.event && <p>Event is required</p>}
-
-              {/* <input
+                {/* <input
               {...register("price", { required: true })}
               value={
                 watch("event")
@@ -94,20 +102,24 @@ const AdventureReservation = ({ events }) => {
                   : ""
               }
             /> */}
-            <label className="font-semibold">Price: </label>
-              <input
-              className="border p-3 rounded-lg "
-                type="text"
-                value={price}
-                readOnly // Make the input field read-only
-              />
+                <label className="font-semibold">Price: </label>
+                <input
+                  className="border p-3 rounded-lg "
+                  type="text"
+                  value={price}
+                  readOnly // Make the input field read-only
+                />
 
-              <input className=" p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80" type="submit" />
-            </div>
-          </form>
+                <input
+                  className=" p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+                  type="submit"
+                />
+              </div>
+            </form>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 export default AdventureReservation;
